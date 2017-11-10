64ac83be8e53244b57c91f04354a0aa88d6576f2
gpark <phg1405@gmail.com>
2017-11-10 18:28:31 +0900

    add input file name

--- cvs2xlsx.py
+++ cvs2xlsx.py
@@ -1,21 +1,43 @@
 import os
-import glob
 import csv
 import pip
-import codecs 
+from glob import glob
 
-is_not_installed = pip.main(['list', 'xlsxwriter'])
+# install xlsxwriter
+is_not_installed = pip.main(['show', 'xlsxwriter'])
 if is_not_installed == 1:
     pip.main(['install', '--user', 'xlsxwriter'])
 
 from xlsxwriter.workbook import Workbook
 
-for csvfile in glob.glob(os.path.join('.', '*.csv')):
+# csv to xlsx
+def all_csv_to_xlsx():
+    for csvfile in glob(os.path.join('.', '*.csv')):
+        workbook = Workbook(csvfile[:-4] + '.xlsx')
+        sheet = workbook.add_worksheet()
+        with open(csvfile, 'rt', encoding='utf8') as f:
+            reader = csv.reader(f)
+            for r, row in enumerate(reader):
+                for c, col in enumerate(row):
+                    sheet.write(r, c, col)
+        workbook.close()
+
+def csv_to_xlsx(file_path):
+    csvfile = os.path.join('.', file_path)
     workbook = Workbook(csvfile[:-4] + '.xlsx')
-    worksheet = workbook.add_worksheet()
+    sheet = workbook.add_worksheet()
     with open(csvfile, 'rt', encoding='utf8') as f:
         reader = csv.reader(f)
         for r, row in enumerate(reader):
             for c, col in enumerate(row):
-                worksheet.write(r, c, col)
+                sheet.write(r, c, col)
     workbook.close()
+
+if __name__ == '__main__':
+    print('[csv to xlsx]')
+    val = input('Input file path(If empty, convert all csv file) : ');
+    if str(val).strip() != '':
+        csv_to_xlsx(val)
+    else:
+        all_csv_to_xlsx()
+    print('Completed!!')
